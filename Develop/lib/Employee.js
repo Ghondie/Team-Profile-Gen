c7cc08439573f80bc7ff045a8c980bae6a56f5e4
Jared Schau <jaredschau@MacBook-Pro.local>
2020-07-23 20:19:59 -0600

    added classes

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+
+//employee is the "base class" everything inherits this class then adds on to it
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return "Employee";
+    }
+}
+
+module.export = Employee;
